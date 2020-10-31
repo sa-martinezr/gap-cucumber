6d363ee20f7d08c132340dc46dc2fb7bb24b5a5e
SAM <sandresmartinezr@gmail.com>
2020-10-30 22:19:47 -0500

    Add userInfo element using css selector to get the element the contains user name

--- pages/Home.js
+++ pages/Home.js
@@ -6,6 +6,10 @@ class Home {
     get siteLogo() {
         return $('#logo_text > h1:nth-child(1) > a:nth-child(1) > img:nth-child(1)');
     }
+
+    get userInfo() {
+        return $('#user_information > span:nth-child(1)');
+    }
 }
 
 module.exports = new Home();
