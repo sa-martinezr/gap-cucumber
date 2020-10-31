ebc88e953751c8dbb4f2920add6085804432e0f0
SAM <sandresmartinezr@gmail.com>
2020-10-31 09:08:30 -0500

    Create support/assertion to check that the successful message after create an employee is present in the page

---
+++ support/assertions/checkSuccesfulCreatedMessage.js
@@ -0,0 +1,9 @@
+import showEmployee from "../../pages/ShowEmployee";
+import assert from 'assert';
+
+export default message => {
+    assert(
+        showEmployee.successfulCreatedMessage.getText() === message,
+        `The message "${message}" is not present in the page`
+    );
+};
