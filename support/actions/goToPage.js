9c840a840ec4498ea1c79a9f3e12754ea64358cc
SAM <sandresmartinezr@gmail.com>
2020-10-31 08:58:43 -0500

    Add new case to manage the new employee url on the goToPage.js action

--- support/actions/goToPage.js
+++ support/actions/goToPage.js
@@ -1,10 +1,13 @@
 import login from "../../pages/Login";
+import newEmployee from "../../pages/NewEmployee";
 
 export default page => {
     switch(page) {
         case "Login":
             browser.url(login.url);
             break;
+        case "New Employee":
+            browser.url(newEmployee.url);
         default:
             console.log(`Invalid page ${page}`);
     }
