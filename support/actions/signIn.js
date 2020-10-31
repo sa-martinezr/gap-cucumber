48c48e0bb188f17037b18a0e33e71ef991f23ffb
SAM <sandresmartinezr@gmail.com>
2020-10-30 19:40:24 -0500

    Create suport/action/signIn.js action to manage the signIn process on the login page

---
+++ support/actions/signIn.js
@@ -0,0 +1,8 @@
+import login from "../../pages/Login";
+import credentials from "../../data/credentials";
+
+export default email => {
+    const password = credentials.find(creds => creds.email === email).password;
+
+    login.signIn(email, password);
+};
