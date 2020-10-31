6a2ee9e060f561da3fc21ebe4979f8e6d5bb502e
SAM <sandresmartinezr@gmail.com>
2020-10-30 22:51:08 -0500

    Create then step to verify the banner with the message Signed in successfully. is present in the page

--- steps/Login/then.js
+++ steps/Login/then.js
@@ -1,6 +1,7 @@
 import { Then } from 'cucumber';
 import checkSiteLogo from "../../support/assertions/checkSiteLogo";
 import checkUserInfo from "../../support/assertions/checkUserInfo";
+import checkSuccesfulSignedInMessage from "../../support/assertions/checkSuccesfulSignedInMessage";
 
 Then('the page should contain the site logo', () => {
     checkSiteLogo();
@@ -8,4 +9,8 @@ Then('the page should contain the site logo', () => {
 
 Then('the page should contain the {string} of the user signed in', (userName) => {
     checkUserInfo(userName);
+});
+
+Then('the page should contain a banner with the message {string}', message => {
+    checkSuccesfulSignedInMessage(message);
 })
