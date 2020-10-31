70aefc719f5f6cc08848bb1b01ea0983a5dbd008
SAM <sandresmartinezr@gmail.com>
2020-10-30 21:55:22 -0500

    Create support/assertions/checkSiteLogo to verify the img path is the same rendered on the home page

---
+++ support/assertions/checkSiteLogo.js
@@ -0,0 +1,10 @@
+import home from "../../pages/Home";
+import assert from 'assert';
+
+export default () => {
+    
+    assert(
+        home.siteLogo.getAttribute('src') ===  `${process.env.BASE_URL}/assets/site_title-34b060c1996fa36129429fcfb794655a.png`,
+        "The site logo is not the original one"
+    );
+};
