29c72e46ec31a87d014d7840efc7baeb07170537
SAM <sandresmartinezr@gmail.com>
2020-10-30 22:22:49 -0500

    Create then assertion to validate that the name of the user signed in is the same shown in the page

--- steps/Login/then.js
+++ steps/Login/then.js
@@ -1,6 +1,11 @@
 import { Then } from 'cucumber';
 import checkSiteLogo from "../../support/assertions/checkSiteLogo";
+import checkUserInfo from "../../support/assertions/checkUserInfo";
 
 Then('the page should contain the site logo', () => {
     checkSiteLogo();
 });
+
+Then('the page should contain the {string} of the user signed in', (userName) => {
+    checkUserInfo(userName);
+})
