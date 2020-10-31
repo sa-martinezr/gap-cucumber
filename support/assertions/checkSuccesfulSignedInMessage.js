71cacc58d58904e1c062ae457312c741b28251e6
SAM <sandresmartinezr@gmail.com>
2020-10-30 22:50:38 -0500

    Create assertion to verify the successful message is the same shown in the page

---
+++ support/assertions/checkSuccesfulSignedInMessage.js
@@ -0,0 +1,9 @@
+import home from "../../pages/Home";
+import assert from 'assert';
+
+export default message => {
+    assert(
+        home.successfulSignedInMessage.getText() === message,
+        `The message "${message}" is not present in the page`
+    );
+};
