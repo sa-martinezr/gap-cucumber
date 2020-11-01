3d4b7b3e2bf0f56fed9a5656fa96f52298cd1715
SAM <sandresmartinezr@gmail.com>
2020-11-01 08:25:08 -0500

    Create two support/actions. The first to return a date with the format present in the employeeList page. The second action is to manage the dataTable in the .features file in order to return an employee object, this action receive the table hashes, a boolean to check is split the date, and a boolean to add the email

---
+++ support/actions/formatDate.js
@@ -0,0 +1,8 @@
+/**
+ * Returns the date in format MM/dd/YYYY
+ * @param {string} dateToTransform
+ */
+export default dateToTransform => {
+    const transformedDate = dateToTransform.split('-');
+    return `${transformedDate[1]}/${transformedDate[0]}/${transformedDate[2]}`;
+};
---
+++ support/actions/transformDataTableToObject.js
@@ -0,0 +1,43 @@
+import formatDate from "./formatDate";
+
+/**
+ * Returns a new employee object
+ * 
+ * @param {Array} dataTableHashes pass the table.hashes()
+ * @param {boolean} splittedDate if the date must be splitted or returned in format MM/dd/YYYY 
+ */
+export default (dataTableHashes, splittedDate = true, addEmail = true)  => {
+    const data = dataTableHashes[0];
+    let employee = {
+        'firstName'         : data['First name'],
+        'lastName'          : data['Last name'], 
+        'identification'    : data['Identification'],
+        'leaderName'        : data['Leader name'],
+    }
+
+    if(addEmail){
+        employee = {
+            ...employee,
+            'email' : data['Email'],
+        };
+    }
+
+    if(splittedDate){
+        const startWorkingDate = data['Start working on'].split('-');
+        const startWorkingDay = startWorkingDate[0];
+        const startWorkingMonth = parseInt(startWorkingDate[1],10);
+        const startWorkingYear = startWorkingDate[2];
+
+        return {
+            ... employee,
+            'startWorkingYear'  : startWorkingYear,
+            'startWorkingMonth' : startWorkingMonth,
+            'startWorkingDay'   : startWorkingDay,
+        };
+    }
+    
+    return {
+        ... employee,
+        'startWorkingOn': formatDate(data['Start working on'])
+    }
+};
