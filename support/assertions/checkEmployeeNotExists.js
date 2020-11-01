d028af09140800310bc5390f5c0c4e4e81ba1349
SAM <sandresmartinezr@gmail.com>
2020-11-01 11:50:01 -0500

    Add browser.pause() before perform the assertion, due the firefox browser runs the assertion step before the page is loaded

--- support/assertions/checkEmployeeNotExists.js
+++ support/assertions/checkEmployeeNotExists.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-undef */
 import employeeList from '../../pages/EmployeeList';
 import assert from 'assert';
 
 export default employee => {
+  browser.pause(1000);
   assert(employeeList.findEmployee(employee) < 0,
     'The employee is present on the page'
   );
