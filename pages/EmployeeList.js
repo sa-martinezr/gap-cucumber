f6fe13737c9fd3ec9eedd79fe6e9ad70ce9ae1bb
SAM <sandresmartinezr@gmail.com>
2020-11-01 08:29:42 -0500

    Improve the employees method to return the rows of the html table. Add methods to find an employee, to find the delete button and the delete employee action in order to find the employee and delete it from the table

--- pages/EmployeeList.js
+++ pages/EmployeeList.js
@@ -5,7 +5,24 @@ class EmployeeList {
     }
 
     get employees() {
-        return $('#content > table:nth-child(3) > tbody');
+        return $$('#content > table:nth-child(3) > tbody > tr:not(:first-child)');
+    }
+
+    findDeleteButton(employeeIndex) {
+      //Add +2 due the table starts from 1 but the index is given from 0, and the first row is the header
+        return $(`#content > table:nth-child(3) > tbody:nth-child(1) > tr:nth-child(${employeeIndex+2}) > td:nth-child(9) > a:nth-child(1)`)
+    }
+
+    findEmployee(employeeToFind) {
+        const employeeToFindArray = Object.values(employeeToFind);
+        return this.employees.findIndex(employee => employeeToFindArray.every(columnValue => employee.getText().includes(columnValue)));
+    }
+
+    deleteEmployee(employeeToDelete) {
+        const employeeIndex = this.findEmployee(employeeToDelete);
+        const deleteButton = this.findDeleteButton(employeeIndex);
+        deleteButton.click();
+        browser.acceptAlert();
     }
 
 }
