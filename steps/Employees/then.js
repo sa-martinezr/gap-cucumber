271ebe869592f3314ba8d21579d026b028f53dd2
SAM <sandresmartinezr@gmail.com>
2020-11-01 08:42:29 -0500

    Change the then step for the create employee feature in order to call the transformDataTableToOjbect action and use the checkEmployeeExists assertion. Add the then step to check that the employee deleted is not present in the page using the checkEmployeeNotExists assertion

--- steps/Employees/then.js
+++ steps/Employees/then.js
@@ -1,24 +1,20 @@
 import { Then } from 'cucumber';
 import goToPage from '../../support/actions/goToPage';
+import transformDataTableToOjbect from "../../support/actions/transformDataTableToObject";
 import checkSuccessfulCreatedMessage from "../../support/assertions/checkSuccesfulCreatedMessage";
-import checkEmployeeCreated from "../../support/assertions/checkEmployeeCreated";
+import checkEmployeeExists from "../../support/assertions/checkEmployeeExists";
+import checkEmployeeNotExists from "../../support/assertions/checkEmployeeNotExists";
+
 
 Then('the page should contain the message {string}', message => {
     checkSuccessfulCreatedMessage(message);
 });
 
 Then('the new employee information is visible at the {string} page with the following data:', (page, table) => {
-    goToPage(page);
-    const data = table.hashes();
-    const startWorkingDate = data[0]['Start working on'].split('-');
-    const newEmployee = {
-        'firstName'     : data[0]['First name'],
-        'lastName'      : data[0]['Last name'],
-        'identification': data[0]['Identification'],
-        'leaderName'    : data[0]['Leader name'],
-        'startWorkingOn': `${startWorkingDate[1]}/${startWorkingDate[0]}/${startWorkingDate[2]}`
-    }
+    goToPage(page);    
+    checkEmployeeExists(transformDataTableToOjbect(table.hashes(),false,false));
+});
 
-    checkEmployeeCreated(newEmployee);
-
+Then('the employee information is not present on the page', table => {
+    checkEmployeeNotExists(transformDataTableToOjbect(table.hashes(),false,false));
 });
