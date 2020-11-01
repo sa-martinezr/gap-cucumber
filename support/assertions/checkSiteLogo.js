20deaaa78313c3a9984d3b8e59c4e1c90e8cb213
SAM <sandresmartinezr@gmail.com>
2020-11-01 11:48:56 -0500

    Fix issue with support/assertion to verify the logo on the site, because firefox doesn't return the complete path. Change the validation to check that the img name is included on the src attribute

--- support/assertions/checkSiteLogo.js
+++ support/assertions/checkSiteLogo.js
@@ -3,7 +3,7 @@ import assert from 'assert';
 
 export default () => {
   assert(
-    home.siteLogo.getAttribute('src') === `${process.env.BASE_URL}/assets/site_title-34b060c1996fa36129429fcfb794655a.png`,
-    'The site logo is not the original one'
+    home.siteLogo.getAttribute('src').includes('/assets/site_title-34b060c1996fa36129429fcfb794655a.png'),
+    'The site logo is not the original one '
   );
 };
